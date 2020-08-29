9df5d332e7180f47b7dc0e08e7903e84b05ba5ee
ZachWang <815810395@qq.com>
2020-08-29 10:32:35 +0800

    Update GetSetVel.m

--- GetSetVel.m
+++ GetSetVel.m
@@ -1,6 +1,6 @@
 function [w0, ws] = GetSetVel(T, D, rou_s, c)
 %w0 - settling velocity in clear water; ws - settling velocity in muddy
-%water
+%water; D - diameter (m); c - volumetric concentration (-)
 %calculate the settling velocity of sediment
 
 niu=GetKvisc(T);
@@ -12,7 +12,7 @@ a3=13.95*niu/D;
 w0=sqrt(a1+a2)-a3;
 
 rp=w0*D/niu;
-n=(4.7+0.41*rp^0.75)/(1+0.175*rp^0.75);
+n=(4.7+0.41*rp^0.75)/(1+0.175*rp^0.75);    %Eq. (6) in "Sediment entrainment at high flow velocity"
 ws=w0*(1-c)^n;
 
 end
