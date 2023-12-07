34bdb39680eaac5cd07df9746a690702d23d0310
ZachWang <815810395@qq.com>
2023-12-07 18:41:17 +0800

    参考流速分布来拟合

--- StepType/findBestParas2.m
+++ StepType/findBestParas2.m
@@ -1,4 +1,4 @@
-%使用多项式拟合
+%使用类似异重流流速分布曲线形式的公式拟合切应力分布
 % 假设您的数据已经以表格形式导入，命名为 data
 % 分离数据
 load('data_smaller_than_Bw.mat');
@@ -13,7 +13,8 @@ objectiveFunction = @(parameters) optimizeRelativeShear(parameters, Da, pis, tau
 % 初始参数猜测
 exps_ini = [-1.295, 0.026, 0.221, -1.062];
 coef_ini = 0.025;
-initialGuess = [exps_ini, coef_ini];
+aofx_ini = 1;
+initialGuess = [exps_ini, coef_ini, aofx_ini];
 
 % 进行优化（使用 MATLAB 优化工具箱中的函数，如 fmincon）
 optimizedParameters = fmincon(objectiveFunction, initialGuess, [ ], [ ]);
@@ -21,16 +22,21 @@ optimizedParameters = fmincon(objectiveFunction, initialGuess, [ ], [ ]);
 % 优化后的 exps 和 coef
 optimizedExps = optimizedParameters(1:4);
 optimizedCoef = optimizedParameters(5);
+optimizedAofx = optimizedParameters(6);
 
 % 使用优化后的参数计算 tauRel
 tauRelOptimized = RelativeShear(Da, pis, tau, optimizedExps, optimizedCoef);
 
 % 模型评估
 % 计算 R? 或其他统计指标
-curvefit = fit(zp, tauRelOptimized, 'poly3');
 y = linspace(0.01, max(zp), 100);
 y = y.';
-x = curvefit( y );
+
+p_distri=[0.6; 2.2];     %initial guess
+zpp = zp.^optimizedAofx./(zp.^optimizedAofx+1);
+[a]=nlinfit(zpp, tauRelOptimized, @tau_distribution, p_distri);
+x=tau_distribution(a, zpp);
+
 
 plot(tauRelOptimized,zp,'ro');
 hold on;
@@ -40,13 +46,27 @@ plot(x, y);
 function cost = optimizeRelativeShear(parameters, Da, pis, tau, zp)
     exps = parameters(1:4);
     coef = parameters(5);
+    aofx = parameters(6);
     tauRelCalculated = RelativeShear(Da, pis, tau, exps, coef);
     
-    % 假设我们使用 comb1 来预测 tauRelCalculated
     % 这里需要定义一个合适的预测模型
-    curvefit = fit(zp, tauRelCalculated, 'poly2');
-    tauRelPredicted = curvefit( zp ); % 依据 comb1, comb2, comb3 等
+    
+    p_distri=[0.6; 2.2];     %initial guess
+    zpp = zp.^aofx./(zp.^aofx+1);       %把zp映射到(0, 1)区间
+    [a]=nlinfit(zpp, tauRelCalculated, @tau_distribution, p_distri);
+    
+    tauRelPredicted=tau_distribution(a, zpp);
 
     % 计算成本（比如均方误差）
     cost = mean((tauRelCalculated - tauRelPredicted).^2);
+end
+
+
+function [tau_tauMax]=tau_distribution(a, eta)
+
+m = a(1);
+n = a(2);
+sigma=(m+n)^(m+n)/m^m/n^n;
+
+tau_tauMax=sigma*eta.^m.*(1-eta).^n;
 end
